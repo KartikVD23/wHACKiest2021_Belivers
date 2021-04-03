24cc969f0178c5e523ca80eabe1e1474edab1f7a
yvulture-7 <72075894+yvulture-7@users.noreply.github.com>
2021-04-03 22:39:49 +0530

    Update navbar.component.js

--- frontend/src/components/navbar.component.js
+++ frontend/src/components/navbar.component.js
@@ -1,38 +1,42 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
+import {Navbar,Nav,Button} from 'react-bootstrap';
+
 export default class NavigationBar extends Component {
+ 
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">
-          GYM
-        </Link>
-        <div className="collpase navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="navbar-item">
-              <Link to="/" className="nav-link">
-                Home
-              </Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/admin-login" className="nav-link">
-                Admin Login
-              </Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/member-login" className="nav-link">
-                Member Login
-              </Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/member-signup" className="nav-link">
-                Member Sign Up
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+     <div >
+     <Navbar collapseOnSelect expand="lg" bg="dark" variant="secondary">
+     <Link to="/"><Navbar.Brand href="#home">Gym</Navbar.Brand></Link> 
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+       <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+           <Nav.Link>Features</Nav.Link>
+        
+        
+        <Nav.Link href={"/pricing.jsx"}>Pricing</Nav.Link>
+        
+       </Nav>
+       <Nav>
+         <Link to="/admin-login">
+       <Button onClick="admin" className="btn" variant="outline-warning" size="sm" onClick="adminpage">Admin-Login</Button>{' '}
+       </Link><Link to="member-signup"><Button className="btn" variant="outline-primary" size="sm" href="">Signup</Button>{' '}
+       </Link><Link to="member-login"><Button variant="dark" size="sm" href="">Login</Button>{' '}
+       </Link>
+       </Nav>
+   </Navbar.Collapse>
+   
+   </Navbar>
+   
+</div>
+
+
+
+
+
+
     );
   }
 }
