ed6472a4cd2634ecba24ed89eb9ad0b50a6d88c1
yvulture-7 <72075894+yvulture-7@users.noreply.github.com>
2021-04-03 22:37:57 +0530

    Create header.jsx

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button } from 'react-bootstrap';
+import  "./header.css"
+function header() {
+    return (
+        <div  className="header">
+         
+        <h1 > Gym Booking System</h1> 
+        <p>get your gym to full capacity</p>
+         
+         
+
+        </div>
+    )
+}
+
+export default header
